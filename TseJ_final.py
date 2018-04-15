b6ce06356244020c400f09180ff3f54a967caf46
jatse <jatse@csumb.edu>
2018-04-15 00:01:48 -0700

    implemented axe throwing

--- TseJ_final.py
+++ TseJ_final.py
@@ -6,7 +6,6 @@
 #-------------------------------------------------
 import time       #imported for animations
 import random     #imported for random generation of game elements
-import threading  #imported for running concurrent events
 currentDirectory = __file__[:-13] #removes this file's name from file path to get current directory
 setMediaFolder(currentDirectory)
 
@@ -32,8 +31,6 @@ class Sprite(object):
   
   #returns coordinate of somewhere closer to the prey, if not obstructed
   def hunt(self, prey, hitMap):
-    coordX = 0
-    coordY = 0 
     clearN = false
     clearS = false
     clearE = false
@@ -79,18 +76,97 @@ class Player(Sprite):
     Sprite.__init__(self, intX, intY)
     self.sprite = makePicture("images/player_sprite.jpg")
     self.color = red
+    self.axeCount = 5
+    
+  def throwAxe(self, enemies, enemyCount, updateMap, hitMap, grassMap, xDelta, yDelta, renderCoord):
+    #use axe
+    self.axeCount -= 1
+    currentAxeSprite = 1
+    
+    #throw up to 5 spaces
+    for i in range(5):
+      xDistance = self.x + (xDelta * (i + 1))
+      yDistance = self.y + (yDelta * (i + 1))
+      #if beyond bounds, stop throw.
+      if xDistance > 778 or xDistance < 0 or yDistance < 32 or yDistance > 512:
+        return enemyCount
+      #if not an obstacle, paste axe in direction. farther with each loop.
+      if not collisionCheck(hitMap, self.sprite, xDistance, yDistance, black):
+        #then paste axe there and render. if item there, skip render and check.
+        if not collisionCheck(hitMap, self.sprite, xDistance, yDistance, green):
+          pasteToMap(updateMap, makePicture("images/axe_sprite" + str(currentAxeSprite) + ".jpg"), xDistance, yDistance, white)
+          drop(renderCoord, updateMap)
+          time.sleep(1)  #slow down for visible animation
+          #check if enemy was hit
+          if collisionCheck(hitMap, self.sprite, xDistance, yDistance, blue):
+            #if so, find enemy
+            for i in range(enemyCount):
+              if enemies[i].x == xDistance and enemies[i].y == yDistance:
+                #wipe enemy from maps
+                pasteToMap(hitMap, makeEmptyPicture(32, 32, white), xDistance, yDistance)
+                grassPatch(grassMap, updateMap, xDistance, yDistance)
+                drop(renderCoord, updateMap)
+                #remove from enemy list, update count variable, and stop throw
+                del enemies[i]
+                return enemyCount - 1;
+          else: #if no enemy was hit, remove axe
+            grassPatch(grassMap, updateMap, xDistance, yDistance)
+            drop(renderCoord, updateMap)
+          #rotate axe sprite
+          if currentAxeSprite == 3:
+            currentAxeSprite = 1
+          else:
+            currentAxeSprite += 1
+      else:
+        return enemyCount  #if obstacle encountered, end throw.
     
 class Wolf(Sprite):
   def __init__(self, intX = 0, intY = 0):
     Sprite.__init__(self, intX, intY)
     self.sprite = makePicture("images/wolf_sprite.jpg")
+    self.nameType = "wolf"
     self.color = blue
+  
+  #wolf will wander 10% of the time   
+  def wander(self, prey, hitMap):
+    clearN = false
+    clearS = false
+    clearE = false
+    clearW = false
+    
+    #checks if cardinal directions are available
+    if self.x - 32 > 0 and (collisionCheck(hitMap, self.sprite, self.x - 32, self.y, white) or collisionCheck(hitMap, self.sprite, self.x - 32, self.y, red)):
+      clearW = true
+    if self.x + 32 < 778 and (collisionCheck(hitMap, self.sprite, self.x + 32, self.y, white) or collisionCheck(hitMap, self.sprite, self.x + 32, self.y, red)):
+      clearE = true
+    if self.y - 32 > 32 and (collisionCheck(hitMap, self.sprite, self.x, self.y - 32, white) or collisionCheck(hitMap, self.sprite, self.x, self.y - 32, red)):
+      clearN = true
+    if self.y + 32 < 512 and (collisionCheck(hitMap, self.sprite, self.x, self.y + 32, white) or collisionCheck(hitMap, self.sprite, self.x, self.y + 32, red)):
+      clearS = true
+      
+    #if surrounded by obstacles, stay
+    if clearN == false and clearE == false and clearS == false and clearW == false:
+      return 0, 0
+      
+    #else, go in random direction  
+    while true:
+      lottery = random.randint(1,4)
+      if lottery == 1 and clearW:
+        return -32 , 0
+      elif lottery == 2 and clearE:
+        return 32 , 0
+      elif lottery == 3 and clearN:
+        return 0 , -32
+      elif lottery == 4 and clearS:
+        return 0 , 32
   
 class Bear(Sprite):
   def __init__(self, intX = 0, intY = 0):
     Sprite.__init__(self, intX, intY)
     self.sprite = makePicture("images/bear_sprite.jpg")
+    self.nameType = "bear"
     self.color = blue
+    self.hp = 2
         
 #--------------------------
 #----- MAIN GAME LOOP -----
@@ -162,30 +238,40 @@ def main():
   enemies = []                                                                                                #holds enemy objects (max 10)
   enemyCount = 0
   for i in range(3):
-    enemies.append(Wolf(random.randrange(0, 778, 32), random.randrange(32, 512, 32)))                            #creates wolf object with random starting coordinates
+    enemies.append(Wolf(random.randrange(0, 778, 32), random.randrange(32, 512, 32)))                         #creates wolf object with random starting coordinates
     while not collisionCheck(hitMap, enemies[i].sprite, enemies[i].x, enemies[i].y, white):                   #make sure wolf is on empty space
       enemies[i].x = random.randrange(0, 778, 32)
       enemies[i].y = random.randrange(32, 512, 32)
     enemies[i].move(updateMap, hitMap, grassMap, 0, 0)                                                        #add wolf to map
     enemyCount += 1
+    
+  #prepare axe UI graphics
+  uiAxe = makePicture("images/axe_sprite1.jpg")
+  noAxe = makeEmptyPicture(32, 32, black)
   
   #--- MAIN GAME LOOP ---
   turnCount = 0   #used to trigger in game events
   
   while true:
-    #- 1. render graphics   
-    drop(renderCoord, updateMap)                                                    #output to screen
-    #repaint(hitMap)                                                                 #DEBUG: show hit map
+    #- 1. update UI and render graphics 
+    for i in range(10):                                               #refresh axe count UI
+      if player.axeCount > i:
+        pasteToMap(updateMap, uiAxe, 32 * i, 0, white)
+      else: 
+        pasteToMap(updateMap, noAxe, 32 * i, 0, white)
+        
+    drop(renderCoord, updateMap)                                      #output to screen
+    #repaint(hitMap)                                                  #DEBUG: show hit map
     
     #- 2. get user input
-    while true:                                                                    #keep getting input until input matches valid commands    
+    while true:                                                       #keep getting input until input matches valid commands    
       userInput = requestString("What would you like to do?")
       userInput.strip().lower()
-      if userInput in ["n","s","e","w","north","south","east","west","r","rest"]:    
+      if userInput in ["n","s","e","w","north","south","east","west","r","rest","a","axe"]:    
         break
         
     #- 3. execute player's turn
-    successfulMove = false  #if player runs into obstacle, turn does not count
+    successfulTurn = false  #if player runs into obstacle or doesn't throw axe, turn does not count
     
     #movement check. if move command in bounds and space is free, then allow move
     if userInput in ["n", "north"]:
@@ -208,23 +294,42 @@ def main():
         successfulMove = true   
         
     #axe action
-    
-    
+    if userInput in ["a", "axe"] and player.axeCount > 0:
+      #get direction in which to throw axe
+      #axe will go 5 units out
+      throwDirection = requestString("Which way will you throw?")
+      if throwDirection in ["n", "north"]:
+        enemyCount = player.throwAxe(enemies, enemyCount, updateMap, hitMap, grassMap, 0, -32, renderCoord)
+        successfulMove = true 
+      elif throwDirection in ["s", "south"]:
+        enemyCount = player.throwAxe(enemies, enemyCount, updateMap, hitMap, grassMap, 0, 32, renderCoord)
+        successfulMove = true 
+      elif throwDirection in ["w", "west"]:
+        enemyCount = player.throwAxe(enemies, enemyCount, updateMap, hitMap, grassMap, -32, 0, renderCoord)
+        successfulMove = true 
+      elif throwDirection in ["e", "east"]:
+        enemyCount = player.throwAxe(enemies, enemyCount, updateMap, hitMap, grassMap, 32, 0, renderCoord)
+        successfulMove = true 
+          
     #- 4. execute enemies' turn
-    for i in range(enemyCount):
-      huntX, huntY = enemies[i].hunt(player, hitMap)                #get where enemy should move
-      enemies[i].move(updateMap, hitMap, grassMap, huntX, huntY)    #move
+    if successfulMove:
+      for enemy in enemies:
+        if enemy.nameType == "wolf" and random.randint(0,4) > 0:             #wolves will wander 20% of the time
+          huntX, huntY = enemy.wander(player, hitMap)
+        else:
+          huntX, huntY = enemy.hunt(player, hitMap)                          #get where enemy should move
+        enemy.move(updateMap, hitMap, grassMap, huntX, huntY)                #move
       
-    if collisionCheck(hitMap, player.sprite, player.x, player.y, blue):    #check if player eaten
-      break
+      if collisionCheck(hitMap, player.sprite, player.x, player.y, blue):    #check if player eaten
+        break
     
     #- 5. execute game events
-    turnCount += 1
+    if successfulMove:
+      turnCount += 1
   
   #--- GAME OVER ---
   gameOverScreen = makePicture("images/game_over.jpg")    
   drop(renderCoord, gameOverScreen)
-  showInformation("Press OK to restart.")
   
       
 #---------------------------      
